018137a28aa4be6182a417fceaac1d908a3035f4
maliao292 <maming292@163.com>
2020-11-06 16:25:58 +0800

    Initial Commit

---
+++ request/index.js
@@ -0,0 +1,16 @@
+let defaultUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
+const request = (params) => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      ...params,
+      success(res) {
+        resolve(res.data)
+      },
+      fail(err) {
+        reject(err)
+      }
+    })
+  })
+}
+export const homeSwiper = () =>  request({url:defaultUrl+'/home/swiperdata'})
+export const homeCatitems = () =>  request({url:defaultUrl+'/home/catitems'})
