d5137fbc0e2fc6f96921c9cf083cd71d83afd465
maliao292 <maming292@163.com>
2020-11-12 14:31:48 +0800

    分类

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wxPro
 微信小程序
+
+### 
+```
+操作	异步方法	同步方法
+插入	wx.setStorage	wx.setStorageSync
+读取	wx.getStorage	wx.getStorageSync
+删除	wx.removeStorage	wx.removeStorageSync
+清空	wx.clearStorage	wx.clearStorageSync
+获取缓存信息	wx.getStorageInfo	wx.getStorageInfoSync
+```
--- pages/category/index.js
+++ pages/category/index.js
@@ -1,76 +1,125 @@
 // pages/category/index.js
-import {categoryData} from '../../request/index'
+import { categoryData } from '../../request/index'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    listArr:[],
-    activeMenu:0
-  },
-  choseClass(e){
-    console.log(e.currentTarget.dataset.ind)
-    let activeMenu = e.currentTarget.dataset.ind
-    this.setData({activeMenu})
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    categoryData().then(res=>{
-      this.setData({
-        listArr:res.message
-      })
-    })
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        listArr: [],
+        activeMenu: 0,
+        toView: 'autoId_0',
+        scrp: 0,
+        scrollTopArr: [] // 滚动数据存储
+    },
+    choseClass(e) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+        let activeMenu = e.currentTarget.dataset.ind
+        this.setData({ activeMenu })
+        let query = wx.createSelectorQuery().in(this);
+        this.data.listArr.forEach((res, ind) => {
+            var toViewid = "#autoId_" + ind; //获取id
 
-  },
+            query.select(toViewid).boundingClientRect();
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+        })
 
-  },
+        query.exec((res) => {
+            console.log(res)
+            let arr = []
+            let sum = res.reduce((prev, cur, index, array) => {
+                arr.push(prev)
+                return prev + cur.height
+            }, 0)
+            arr.push(sum)
+            this.setData({ scrollTopArr: arr })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+            console.log(this.data.scrollTopArr)
+            this.setData({ scrp: this.data.scrollTopArr[activeMenu] })
+        });
 
-  },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+        // 
 
-  },
+        // let query = wx.createSelectorQuery().in(this);
+        // query.selectViewport().scrollOffset()
+        // query.select(toViewid).boundingClientRect();
+        // // query.select("#FilterNav").boundingClientRect();
+        // // query.select("#" + ((letter == '#') ? 'other' : letter)).boundingClientRect();
+        // let _this = this
+        // query.exec(function(res) {
+        //     console.log(res[1].top)
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+        //     _this.setData({ scrp: res[1].top })
+        //         // wx.pageScrollTo({
+        //         //     scrollTop: res[1].top,
+        //         //     duration: 0
+        //         // });
+        // });
 
-  }
+
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        categoryData().then(res => {
+            this.setData({
+                listArr: res.message
+            })
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
