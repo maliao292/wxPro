3b4a8d6ab6f116bf426ab3366386cad1e4dc941c
maliao292 <maming292@163.com>
2020-11-17 09:14:04 +0800

    wtf

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,6 @@
-import { goodsList } from '../../request/index'
+import {
+    goodsList
+} from '../../request/index'
 Page({
 
     /**
@@ -17,18 +19,46 @@ Page({
             id: 2,
             name: '价格',
             isActive: false,
-        }]
+        }],
+        proList: [],
+
     },
+    searchData: {
+        cid: null,
+        pagenum: 1,
+        pagesize: 10
+    },
+    isEnd: false,
     tabsItemChange(e) {
         console.log(e.detail.activeInd)
     },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
-        goodsList(options).then(res => {
-            console.log(res)
+    onLoad: function (options) {
+        this.searchData.cid = options.cid
+        goodsList(this.searchData).then(res => {
+            this.setData({
+                proList: res.message.goods
+            })
         })
 
     },
+    onReachBottom: function (e) {
+        if (!this.isEnd) {
+            this.searchData.pagenum++
+            goodsList(this.searchData).then(res => {
+                this.setData({
+                    proList: [...this.data.proList, ...res.message.goods]
+                })
+                let maxPage = Math.ceil(res.message.total / this.searchData.pagesize)
+                if (maxPage <= Number(res.message.pagenum)) {
+                    this.isEnd = true
+
+                }
+
+            })
+        }
+
+    }
 })
